7f73d01bb5f82946a86d21e4f21b06b96cc39de1
Fatih Çakır <ckrfth.s@gmail.com>
2025-11-11 22:51:40 +0300

    initial commit

---
+++ server.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    today = datetime.today()
+    day_name = today.strftime("%A")
+    return render_template("home.html", day=day_name)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
